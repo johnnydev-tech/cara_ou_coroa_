0436eb90ea7b7ebff13405084113bf78ded08935
Johnny Freire <johnnyfreirejh@hotmail.com>
2020-12-01 00:07:48 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:caraoucoroa/Jogar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: Jogar(),
+  ));
+}
